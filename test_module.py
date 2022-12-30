40505f9a09777abfbb52b3584971fb2de0f8e866
Engbor <liak3387@gmail.com>
2022-12-30 17:02:53 +0300

    code according to PEP8

--- test_module.py
+++ test_module.py
@@ -1,6 +1,5 @@
 import os
 
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 dir_files = [filename.lower() for filename in os.listdir(BASE_DIR)]
 
@@ -11,3 +10,5 @@ def test_program():
     for filename in files_list:
         assert filename in dir_files, f'Файл `{filename}` не найден в корне репозитория'
 
+
+        
